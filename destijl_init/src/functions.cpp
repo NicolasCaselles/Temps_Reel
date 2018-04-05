b529d0225fd116fbdca48e63d7caec4903ad9903
davidpuechm <38099020+davidpuechm@users.noreply.github.com>
2018-04-05 13:01:19 +0200

    Update functions.cpp

--- destijl_init/src/functions.cpp
+++ destijl_init/src/functions.cpp
@@ -1,7 +1,11 @@
 #include "../header/functions.h"
 
 char mode_start;
-int compteur;
+
+Camera *camera(0);
+extern Arene *monArene(0);
+extern int arene_validee=0;
+extern int mode_camera=0; 
 
 void write_in_queue(RT_QUEUE *, MessageToMon);
 
@@ -51,11 +55,7 @@ void f_sendToMon(void * arg) {
             printf("%s : message {%s,%s} in queue\n", info.name, msg.header, msg.data);
 #endif
 
-    
-            if(send_message_to_monitor(msg.header, msg.data)<0)
-            {
-                rt_sem_v(&sem_errS);
-            }
+            send_message_to_monitor(msg.header, msg.data);
             free_msgToMon_data(&msg);
             rt_queue_free(&q_messageToMon, &msg);
         } else {
@@ -83,10 +83,6 @@ void f_receiveFromMon(void *arg) {
         printf("%s : waiting for a message from monitor\n", info.name);
 #endif
         err = receive_message_from_monitor(msg.header, msg.data);
-        if(err<0)
-        {
-            rt_sem_v(&sem_errR);
-        }
 #ifdef _WITH_TRACE_
         printf("%s: msg {header:%s,data=%s} received from UI\n", info.name, msg.header, msg.data);
 #endif
@@ -95,8 +91,7 @@ void f_receiveFromMon(void *arg) {
 #ifdef _WITH_TRACE_
                 printf("%s: message open Xbee communication\n", info.name);
 #endif
-                //rt_sem_v(&sem_openComRobot);
-                rt_sem_broadcast(&sem_openComRobot);
+                rt_sem_v(&sem_openComRobot);
             }
         } else if (strcmp(msg.header, HEADER_MTS_DMB_ORDER) == 0) {
             if (msg.data[0] == DMB_START_WITHOUT_WD) { // Start robot
@@ -114,10 +109,18 @@ void f_receiveFromMon(void *arg) {
                 rt_mutex_acquire(&mutex_move, TM_INFINITE);
                 move = msg.data[0];
                 rt_mutex_release(&mutex_move);
+               
 #ifdef _WITH_TRACE_
                 printf("%s: message update movement with %c\n", info.name, move);
 #endif
 
+            }
+        }
+        else if(strcmp(msg.header, HEADER_MTS_CAMERA) == 0)
+        {
+            if (msg.data[0] == CAM_OPEN) {
+                rt_sem_v(&sem_startCam);
+                printf("Message d'ouverture recu\n");
             }
         }
     } while (err > 0);
@@ -194,186 +197,155 @@ void f_startRobot(void * arg) {
 }
 
 void f_move(void *arg) {
- 
     /* INIT */
-  /*  RT_TASK_INFO info;
+    RT_TASK_INFO info;
     rt_task_inquire(NULL, &info);
     printf("Init %s\n", info.name);
-    rt_sem_p(&sem_barrier, TM_INFINITE);*/
+    rt_sem_p(&sem_barrier, TM_INFINITE);
 
-    // /* PERIODIC START */
+    /* PERIODIC START */
 /*#ifdef _WITH_TRACE_
     printf("%s: start period\n", info.name);
-#endif
+#endif*/
     rt_task_set_periodic(NULL, TM_NOW, 100000000);
     while (1) {
-#ifdef _WITH_TRACE_
+/*#ifdef _WITH_TRACE_
         printf("%s: Wait period \n", info.name);
-#endif
+#endif*/
         rt_task_wait_period(NULL);
-#ifdef _WITH_TRACE_
+/*#ifdef _WITH_TRACE_
         printf("%s: Periodic activation\n", info.name);
         printf("%s: move equals %c\n", info.name, move);
-#endif
+#endif*/
         rt_mutex_acquire(&mutex_robotStarted, TM_INFINITE);
         if (robotStarted) {
             rt_mutex_acquire(&mutex_move, TM_INFINITE);
             send_command_to_robot(move);
             rt_mutex_release(&mutex_move);
-#ifdef _WITH_TRACE_
+/*#ifdef _WITH_TRACE_
             printf("%s: the movement %c was sent\n", info.name, move);
-#endif            
-        }
-        rt_mutex_release(&mutex_robotStarted);
-    }*/
-}
-
-void f_batterie(void *arg){
-    printf("\n\nTest \n\n\n");
-    RT_TASK_INFO info;
-    printf("\n\nTest2 \n\n\n");
-    MessageToMon msg;
-    int niveau_bat = 2;
-    char message[10];
-    //message = malloc(sizeof(char)*10);
-    printf("\n\nTest3 \n\n\n");
-    rt_task_inquire(NULL, &info);
-    printf("\n\nInit %s\n\n\n", info.name);
-    rt_sem_p(&sem_barrier, TM_INFINITE);
-    printf("\nEntree dans le while\n\n\n");
-    rt_task_set_periodic(NULL, TM_NOW, 500000000);
-    
-    while(1)
-    {
-        // Timer Période
-        rt_task_wait_period(NULL);
-        rt_mutex_acquire(&mutex_robotStarted, TM_INFINITE); 
-        if(robotStarted == 1){
-            //Envoi du message :
-
-            niveau_bat = send_command_to_robot(DMB_GET_VBAT); 
-            if(niveau_bat < 0)
-            {
-                rt_sem_p(&sem_pbComm,TM_INFINITE);
-            }
-            else
-            {
-                rt_mutex_acquire(&mutex_compteur, TM_INFINITE);
-                compteur = 0;
-                rt_mutex_release(&mutex_compteur);
-            }
-            sprintf(message,"%d",niveau_bat);
-            //printf("Niveau batterie : %d\n",niveau_bat);
-            //fflush(stdout);    
-
-
-            set_msgToMon_header(&msg, HEADER_STM_BAT);
-            set_msgToMon_data(&msg, (void *) message);
-            //write_in_queue(&q_messageToMon, msg);
-            if(send_message_to_monitor(msg.header, msg.data)<0)
-            {
-                rt_sem_v(&sem_errS);
-            }
-        }
-        else {
-            //printf("Robot not started\n");
+#endif            */
         }
         rt_mutex_release(&mutex_robotStarted);
     }
-    
 }
-
-void f_watchComServer(void *arg)
-{
-    rt_sem_p(&sem_barrier, TM_INFINITE);
-    rt_sem_p(&sem_errS, TM_INFINITE);
-    rt_sem_p(&sem_errR, TM_INFINITE);
-    printf("Connexion perdue");
-    rt_mutex_acquire(&mutex_robotStarted, TM_INFINITE);
-    robotStarted = 0;
-    rt_mutex_release(&mutex_robotStarted);
-    rt_mutex_acquire(&mutex_camera, TM_INFINITE);
-    mode_camera = 0;
-    rt_mutex_release(&mutex_camera);
-    printf("Connexion perdue \n");
-    close_server();
-    kill_nodejs();   
-}
-
-
-/*void f_watchDog(void *arg)
-{
-    MessageToMon msg;
-    int compteur;
-    RT_TASK_INFO info;
-    rt_task_inquire(NULL, &info);
-    rt_sem_p(&sem_barrier, TM_INFINITE);
-    printf("Prddzdzdz \n");
-    rt_task_set_periodic(NULL, TM_NOW, 100000000);
-    while(1)
-    {
-        rt_sem_p(&sem_startwithWD,TM_INFINITE);
-        printf("Prob \n");
-        rt_task_wait_period(NULL);
-        compteur=0;
-            while(compteur<4)
-            {
-                rt_task_wait_period(NULL);
-                printf("ProbProb \n");
-                rt_mutex_acquire(&mutex_robotStarted, TM_INFINITE); 
-                if(robotStarted == 1){
-                    if(send_command_to_robot(DMB_RELOAD_WD)!=ROBOT_OK)
-                    {
-                        compteur++;
-                    }
-                }
-                else
-                {
-                    compteur = 0;
-                }
-                rt_mutex_release(&mutex_robotStarted);
-            }
-        
-    // set_msgToMon_header(&msg, HEADER_STM_LOST_DMB);
-     if(send_message_to_monitor(msg.header,NULL)<0)
-     {
-         rt_sem_v(&sem_errS);
-     }
-     printf("ProbProbClaMerde \n");
-     //close_communication_robot();
-    }
-}*/
-void f_watchComRobot(void *arg)
-{
-    MessageToMon msg;
-    RT_TASK_INFO info;
-    rt_sem_p(&sem_barrier, TM_INFINITE);
-    rt_task_inquire(NULL, &info);
-    while(1)
-    {
-        rt_mutex_acquire(&mutex_compteur, TM_INFINITE);
-        compteur =0;
-        rt_mutex_release(&mutex_compteur);
-        while(compteur<4)
-        {
-            rt_sem_p(&sem_pbComm,TM_INFINITE);
-            rt_mutex_acquire(&mutex_robotStarted, TM_INFINITE); 
-            if(robotStarted == 1)
-            {
-                rt_mutex_acquire(&mutex_compteur, TM_INFINITE);
-                compteur++;
-                rt_mutex_release(&mutex_compteur);
-            }
-            rt_mutex_release(&mutex_robotStarted);
-        }
-        printf("ProbProbClaMerde \n");
-    }
-}
-
 
 void write_in_queue(RT_QUEUE *queue, MessageToMon msg) {
     void *buff;
     buff = rt_queue_alloc(&q_messageToMon, sizeof (MessageToMon));
     memcpy(buff, &msg, sizeof (MessageToMon));
     rt_queue_send(&q_messageToMon, buff, sizeof (MessageToMon), Q_NORMAL);
+}
+
+
+
+void f_openCamera (void *arg) {
+    /* INIT */
+    rt_sem_p(&sem_barrier, TM_INFINITE);
+    printf("Attente sem_barriere\n");
+    camera = (Camera *)malloc(sizeof(Camera));
+    RT_TASK_INFO info;
+    rt_task_inquire(NULL, &info);
+    printf("Init %s\n", info.name);
+    printf("Attente sem_startCam\n");
+    rt_sem_p(&sem_startCam, TM_INFINITE);
+    printf("Sem_startCam recu\n");
+    /*if (open_camera(camera)==-1){ //echec ouverture camera
+        printf("Probleme camera \n");
+        MessageToMon msg;
+        set_msgToMon_header(&msg, HEADER_STM_NO_ACK);
+        write_in_queue(&q_messageToMon, msg);
+    }
+    else { //ouverture camera OK
+        printf("Camera ouverte\n");
+        mode_camera=1 ;
+        
+    }*/
+    open_camera(camera);
+    mode_camera=1;
+}
+
+
+void f_camera (void *arg){
+    /* INIT */
+    RT_TASK_INFO info;
+    rt_sem_p(&sem_barrier, TM_INFINITE);
+    rt_task_inquire(NULL, &info);
+    printf("Init %s\n", info.name);
+    //rt_sem_p(&sem_cameraStarted, TM_INFINITE);
+    /* PERIODIC START */
+    rt_task_set_periodic(NULL, TM_NOW, 100000000);
+    Image *image_in;
+    //image_in = malloc(sizeof(Image));
+    Image *image_out;
+    MessageToMon msg;
+    Position *maPosition ;
+    printf("on entre dans le while pour l'acquisition des images\n");
+    while (1) {
+        rt_task_wait_period(NULL);
+        if (mode_camera==1) { //en mode 1 la camera envoie les images (avec arene dessinee dessus si l'arene a ete validee)
+            /*if (get_image(camera,image_in,NULL)==-1) {
+                printf("Erreur capture image\n");
+            }
+            else {
+             */
+            printf("on est avant le get_image\n");
+            get_image(camera,image_in,"/home/bertrana/Documents/4eme_annee/temps_reel/TP_temps_reel/raspberry.jpg");
+            printf("on est après le get_image\n");
+            /*if (arene_validee==1) {
+                draw_arena(image_in, image_out, monArene);
+            }
+            else {*/
+                image_out=image_in;
+            //}
+            set_msgToMon_header(&msg, HEADER_STM_IMAGE);
+            set_msgToMon_data(&msg, image_out);
+            send_message_to_monitor(msg.header,msg.data);
+        }
+        else if (mode_camera==2) { //Calcul de la position et envoi de la position et image avec position dessinee
+            Image *imagePos;
+            MessageToMon pos;
+            get_image(camera,image_in,NULL);
+            if (arene_validee==1) {
+                if (detect_position(image_in, maPosition, monArene)==0) {
+                    printf("Pas de robot detecte\n");
+                }
+                draw_arena(image_in, image_out, monArene);
+            }
+            else {
+                if (detect_position(image_in, maPosition)==0) {
+                    printf("Pas de robot detecte\n");
+                }
+                image_out=image_in;
+            }
+            draw_position(image_out, imagePos, maPosition);
+            set_msgToMon_header(&msg, HEADER_STM_IMAGE);
+            set_msgToMon_data(&msg, imagePos);
+            send_message_to_monitor(msg.header,msg.data);
+            set_msgToMon_header(&pos, HEADER_STM_POS);
+            set_msgToMon_data(&pos, maPosition);
+            send_message_to_monitor(pos.header,pos.data);
+        }
+        else if (mode_camera==3){
+            Arene *rectangle;
+            MessageToMon demande_arene;
+            char *message;
+            get_image(camera,image_in,NULL);
+            detect_arena(image_in, rectangle);
+            set_msgToMon_header(&msg, HEADER_STM_IMAGE);
+            set_msgToMon_data(&msg, image_in);
+            send_message_to_monitor(msg.header,msg.data);
+            char* texte = "Validation de l'arene ? (y/n)\n";
+            set_msgToMon_header(&demande_arene, HEADER_STM_MES);
+            set_msgToMon_data(&demande_arene, (void*) texte);
+            send_message_to_monitor(demande_arene.header,demande_arene.data);
+            receive_message_from_monitor("MSG", message);
+            if (message[0]='y'){
+                arene_validee=1;
+            }
+            else {
+                arene_validee=0;
+            }
+        }
+    }
 }
